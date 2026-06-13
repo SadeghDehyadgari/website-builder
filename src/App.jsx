d8cbec40e1c5f7e1b086771f95213e694f51c585
Sadegh Dehyadgari <sadeghdehyadgari@gmail.com>
2026-06-13 03:57:20 +0330

    chore: initial project setup with vite, react-router, zustand, react-query

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
+import PageBuilder from "./pages/PageBuilder/PageBuilder";
+import PublicView from "./pages/PublicView/PublicView";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/builder/:pageId" element={<PageBuilder />} />
+        <Route path="/:slug" element={<PublicView />} />
+        <Route path="/" element={<PublicView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
